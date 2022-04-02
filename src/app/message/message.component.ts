eea8223c3e66e020540f4c83fa6668ccc764e0eb
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-04-01 22:02:15 -0800

    Create message component

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { StreamMessage } from 'stream-chat-angular';
+
+@Component({
+  selector: 'app-message',
+  template: `
+    <div>
+      <b>{{ message?.user?.name }}</b> {{ message?.text }}
+    </div>
+  `,
+  styles: ['b {margin-right: 4px}'],
+})
+export class MessageComponent {
+  @Input() message: StreamMessage | undefined;
+  constructor() {}
+}
